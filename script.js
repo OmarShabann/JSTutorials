ca631d9f5d26853592dd90a759a6dd7c59188c53
3marshabanx <3marshabanx@gmail.com>
2023-02-06 17:24:00 +0200

    half of the project

---
+++ script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector(".menu"),
+	ul = document.getElementById("elements"),
+	close = document.querySelector(".close");
+
+menu.addEventListener("click", () => {
+	ul.style.display = "flex";
+	ul.style.flexDirection = "column";
+	ul.style.position = "absolute";
+	ul.style.top = "0";
+	ul.style.left = "0";
+	ul.style.width = "100%";
+	ul.style.backgroundColor = "#333";
+	close.style.display = "block";
+	close.style.color = "white";
+	close.style.fontSize = "30px";
+});
+close.addEventListener("click", () => {
+	ul.style.display = "none";
+	close.style.display = "none";
+});
